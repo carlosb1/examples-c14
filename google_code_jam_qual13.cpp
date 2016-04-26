77c5452580fbd311c2a0f2bb9c679af2aee1197a
Carlos B <carlos.baezruiz@gmail.com>
2016-04-26 23:39:32 +0200

    exercises and file to test

--- google_code_jam_qual13.cpp
+++ google_code_jam_qual13.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <cmath>
+#include <string>
 #include <iostream>  // includes cin to read from stdin and cout to write to stdout
 using namespace std;  // since cin and cout are both in namespace std, this saves some text
+
 
 
 int change_base(int value, int base) {
@@ -32,6 +34,17 @@ bool is_prime(int num) {
 
 }
 
+std::string dec_to_bin(unsigned number) {
+
+	if (number == 0) { return "0"; }
+	if (number == 1) { return "1";}
+	if (number % 2 == 0) {
+		return dec_to_bin(number / 2) + "0";
+	} else {
+		return dec_to_bin(number / 2) + "1";
+	}	
+}
+
 
 
 int main() {
@@ -41,38 +54,40 @@ int main() {
 	cin >> j;
 
 	
-	unsigned int shift_count = 0;
-	unsigned int value = (unsigned int)33;
-	
-		cout << "Case #" << n; 
+	unsigned int init_value = ((unsigned int)1<<(n-1))+1;
+	cout << dec_to_bin(init_value) << '\n';
+	cout << "Case #" << t<<":"<<'\n'; 
 
-		int number_of_found= 0;
-		std::vector<int> number_values;
-		while (number_of_found== j) {
-			
-
-			/* find */
-			for (int base = 2; base < 11; base++) {
-				int new_value_base = change_base(value,base);
-				if  (is_prime(new_value_base)) {
-					break;					
-				}
-				number_values.push_back(new_value_base);
-			}
-			if (number_values.size() == 9) {
-				//PRINT IN BINARY
-				//cout <<  value;
-				for (int i = 0; i< (int) number_values.size(); i++){
-					cout <<" "<<number_values[i];	
-				}
-				cout << '\n';
-			}
-					
-			number_values.clear();
+	int number_of_found= 0;
+	std::vector<int> number_values;
+	unsigned int loop_value = init_value;
+	int count = 0;
+	while (number_of_found!=j) {
 				
-			/* next correct binary to analyse*/	
-			value=value | (++shift_count << 1);	
+		/* find */
+		for (int base = 2; base < 11; base++) {
+			int new_value_base = change_base(loop_value,base);
+//			cout << new_value_base << '\n';
+			if  (is_prime(new_value_base)) {
+				break;					
+			}
+			number_values.push_back(new_value_base);
 		}
+		if (number_values.size() == 9) {
+			number_of_found+=1;
+			cout <<  dec_to_bin(loop_value) ;
+			for (int i = 0; i< (int) number_values.size(); i++){
+				cout <<" "<<number_values[i];	
+			}
+			cout << '\n';
+		}
+				
+		number_values.clear();
+				
+		/* next correct binary to analyse*/	
+		loop_value= (++count<<1) | init_value;
+		//cout << dec_to_bin(loop_value) << '\n';
+	}
 
 	
 	/*
