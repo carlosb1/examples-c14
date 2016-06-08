f6029d85019d55862b095b4cd34fb82f3a40bc21
Carlos B <carlos.baezruiz@gmail.com>
2016-06-08 21:05:29 +0200

    Added first test for naive bayes classifier

--- ml/naive_bayes_classifier.h
+++ ml/naive_bayes_classifier.h
@@ -1,42 +1,55 @@
 #ifndef NAIVE_BAYES_CLASSIFIER
 #define NAIVE_BAYES_CLASSIFIER
 #include <map>
+#include <vector>
+#include <string>
+
 namespace ml {
 	
+	typedef std::pair<std::string,float> Sample;
 
-	typedef std::map<std::string,float> Sample;
-	struct GaussianValue{
+	struct GaussianInfo{
 		double mean;
 		double variance;
+		std::vector<float> values;
+		
 	};
-	
+	typedef std::map<std::string,GaussianInfo> Row;
 	class NaiveBayesClassifier {
-		inline void addRow(std::string & key, Sample &values ) {
-			this->table.insert(std::pair<std::string,Sample>(key,values));
-		};
+
 		private:
-			std::multimap<std::string,Sample> table;
-			std::map<std::string,std::map<std::string,GaussianValue>> gaussianTable; 
-		public:
-			void train () {
-				std::map<std::string,std::map<std::string,GaussianValue>>::iterator it;
-				for (auto row: table) {
-					std::string key = row.first;
-					Sample values = row.second;
-					it = gaussianTable.find(key);
-					if (it != gaussianTable.end()) {
-						//exists
-					} else {
-						//no exists
-						//Add values
-						//gaussianTable[it->first]=;
+			std::vector<std::pair<std::string,Sample>> recollectedData; 
+			std::map<std::string,std::map<std::string,GaussianInfo>> gaussianTable; 
+		public:		
+			inline void addRow(std::string & type, Sample & param) {
+				this->recollectedData.push_back(std::make_pair(type,param));
+			};
+			inline GaussianInfo getGaussianInfo(std::string & type, std::string & parameter) {
+				//TODO add checker
+				return gaussianTable[type][parameter];
 
-					}
-				}
 			}
-			void classify (Sample & sample) {
+			//TODO add clean method
+			void train () {
+				for (auto data: this->recollectedData) {
+					std::string key = data.first;
+					Sample sample = data.second;
+				
+					//TODO set up a correct constructor
+					Row row;
+					GaussianInfo gaussianInfo;
+					gaussianInfo.values.push_back(sample.second);
+					gaussianInfo.mean = sample.second;
+					gaussianInfo.variance = sample.second;
+					row[sample.first]= gaussianInfo;
+					this->gaussianTable[key]=row;
+				}
 
-			}	
-	};
-};
+			}
+			std::string predict(Sample & sample) {
+				return std::string();	
+			}
+				
+		};
+}
 #endif 
--- ml/test_classifier.cpp
+++ ml/test_classifier.cpp
@@ -2,8 +2,25 @@
 #include "catch.hpp"
 #include "naive_bayes_classifier.h"
 
-TEST_CASE("Naives Bayers Classifier","[naive_bayes_classifier]") {
+TEST_CASE("Naive Bayers Classifier","[addRowCorrectly]") {
 	ml::NaiveBayesClassifier classifier;
-	REQUIRE(classifier.train()==true);
-}
+	std::string type("male");
+	const float inputData = 5.855;
+	ml::Sample sample = std::make_pair("height",inputData);
+	classifier.addRow(type,sample);
+	classifier.train();
+	std::string param("height");
+	ml::GaussianInfo info = classifier.getGaussianInfo(type,param);
+	REQUIRE(info.mean == inputData);
+	REQUIRE(info.values[0] == inputData);
+	REQUIRE(info.variance ==  inputData);
+		
+	//TODO add test for this
+	//TODO separate tests for this
+	//TODO Change constructor for sample
+//	ml::Sample sampleToPredict = std::make_pair("height",6.0);
+	//TODO Parametri type
+//	std::string type = classifier.predict(sampleToPredict);
+	
+};
 
