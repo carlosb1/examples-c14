e1b8d914f06fe3fe491ffae0afc79d63ca7683d0
Carlos B <carlos.baezruiz@gmail.com>
2016-06-18 00:21:49 +0200

    Refactorization tests to permit setUp and tearDown

--- ml/test_classifier.cpp
+++ ml/test_classifier.cpp
@@ -13,59 +13,54 @@ void insertSamples(ml::NaiveBayesClassifier & classifier,std::string type,  std:
 	}
 }
 
-TEST_CASE("Add row correctly","[naive_bayes_classifier]") {
+TEST_CASE("testing add information","[naive_bayes_classifier]") {
 	ml::NaiveBayesClassifier classifier;
-	
+
+	SECTION("Add row correctly") {
 	std::vector<float> v {1.};
-	insertSamples(classifier,"male","height",v);	
+		insertSamples(classifier,"male","height",v);	
+		
+		//TODO isolate testing
+		classifier.train();
+		ml::GaussianInfo info = classifier.getGaussianInfo("male","height");
+		REQUIRE(info.mean == 1.);
+		REQUIRE(info.values[0] == 1.);
+		REQUIRE(info.variance ==  1.);
+	};
+	SECTION("Add two rows correctly") {
+		std::vector<float> v {1., 1.5};
+		insertSamples(classifier,"male","height",v);	
+		
+		classifier.train();
+		ml::GaussianInfo info = classifier.getGaussianInfo("male","height");	
+		REQUIRE(info.values.size() == 2);
+		REQUIRE(info.mean == 1.25);
+		REQUIRE(info.values[0] == 1);
+		REQUIRE(info.values[1] == 1.5);
+		REQUIRE(info.variance ==  0.25 );	
+	};
 
-	//TODO isolate testing
-	classifier.train();
-	ml::GaussianInfo info = classifier.getGaussianInfo("male","height");
-	REQUIRE(info.mean == 1.);
-	REQUIRE(info.values[0] == 1.);
-	REQUIRE(info.variance ==  1.);
+	SECTION("Add different type of samples for testing test") {
+		std::vector<float> v {1., 1.5};
+		insertSamples(classifier,"male","height",v);	
+		
+		std::vector<float> v2 {1.};
+		insertSamples(classifier,"male","weight",v2);	
+	
+		//TODO add test for not finding type of sample
+	
+		classifier.train();
+		ml::GaussianInfo info = classifier.getGaussianInfo("male","weight");
+	
+		REQUIRE(info.values.size() == 1);
+		
+		REQUIRE(info.values[0] == 1);
+		REQUIRE(info.variance ==  0 );
+		REQUIRE(info.mean ==  1 );
+	};
+
 };
 
 
-TEST_CASE("Add two rows correctly","[naive_bayes_classifier]") {
-	//TODO ADD SETUP
-	ml::NaiveBayesClassifier classifier;
-
-	std::vector<float> v {1., 1.5};
-	insertSamples(classifier,"male","height",v);	
-	
-
-	classifier.train();
-	ml::GaussianInfo info = classifier.getGaussianInfo("male","height");
-	
-	REQUIRE(info.values.size() == 2);
-	REQUIRE(info.mean == 1.25);
-	REQUIRE(info.values[0] == 1);
-	REQUIRE(info.values[1] == 1.5);
-	REQUIRE(info.variance ==  0.25 );
-};
-
-TEST_CASE("Add different type of samples for testing test","[naive_bayes_classifier]") {
-	//TODO ADD SETUP
-	ml::NaiveBayesClassifier classifier;
-
-	std::vector<float> v {1., 1.5};
-	insertSamples(classifier,"male","height",v);	
-	
-	std::vector<float> v2 {1.};
-	insertSamples(classifier,"male","weight",v2);	
-
-	//TODO add test for not finding type of sample
-
-	classifier.train();
-	ml::GaussianInfo info = classifier.getGaussianInfo("male","weight");
-	
-	REQUIRE(info.values.size() == 1);
-
-	REQUIRE(info.values[0] == 1);
-	REQUIRE(info.variance ==  0 );
-	REQUIRE(info.mean ==  1 );
-};
 
 
