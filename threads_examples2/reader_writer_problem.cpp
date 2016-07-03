c10c88cb4f96b45ec19fe8c4bf10680b22528238
Carlos B <carlos.baezruiz@gmail.com>
2016-07-03 16:30:13 +0200

    Fixed problems to work with locks, it doesn't fix starvation problem

--- threads_examples2/reader_writer_problem.cpp
+++ threads_examples2/reader_writer_problem.cpp
@@ -10,18 +10,16 @@ std::atomic_bool finish_writer(false);
 
 
 void reader() {
-
 	while (!finish_reader) {
 		std::cout  <<"I am reading" << '\n';
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+//		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 	std::cout << "It is finished reader" << '\n';
-
 }
 void writer() {
 	while(!finish_writer) {
 		std::cout << "I am writing" << '\n';
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+//		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 	std::cout << "It is finished writer" << '\n';
 }
@@ -45,30 +43,34 @@ std::mutex mutex_reader;
 std::mutex mutex_writer;
 
 int count_readers=0;
-void multiple_lock_reader() {
+void multiple_lock_reader(int num_reader) {
 	
-	mutex_reader.lock();
-	count_readers++;
-	if (count_readers==1) {
-		mutex_writer.lock();	
-	}
-	mutex_reader.unlock();	
-	reader();
+	while (!finish_reader) {
+		mutex_reader.lock();
+		count_readers++;
+		if (count_readers==1) {
+			mutex_writer.lock();	
+		}
+		mutex_reader.unlock();	
+		
+		std::cout  <<"I am reading  reader: "<< num_reader << '\n';
 	
-	mutex_reader.lock();
-	count_readers--;
-	if (count_readers==0) {
-		mutex_writer.unlock();	
+		mutex_reader.lock();
+		count_readers--;
+		if (count_readers==0) {
+			mutex_writer.unlock();	
+		}
+		mutex_reader.unlock();	
 	}
-	mutex_reader.unlock();	
-
 
 
 }
 void multiple_lock_writer() {
-	mutex_writer.lock();
-	writer();
-	mutex_writer.unlock();
+	while(!finish_writer) {
+		mutex_writer.lock();	
+		std::cout << "I am writing" << '\n';
+		mutex_writer.unlock();
+	}
 
 }
 
@@ -96,13 +98,17 @@ void versionLockSync() {
 }
 
 void versionMultipleLockSync() {
-	std::thread tReader(multiple_lock_reader);
+	std::thread tReader(multiple_lock_reader,1);
+	std::thread tReader2(multiple_lock_reader,2);
+	std::thread tReader3(multiple_lock_reader,3);
 	std::thread tWriter(multiple_lock_writer);
 	std::this_thread::sleep_for(std::chrono::seconds(4));
 		
 	finish_reader = true;
 	finish_writer = true;
 	tReader.join();
+	tReader2.join();
+	tReader3.join();
 	tWriter.join();
 }
 
