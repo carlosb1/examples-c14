237acabfc57f1a0e13c6ecadec1b46f75adf9902
Carlos B <carlos.baezruiz@gmail.com>
2017-06-20 01:00:33 +0200

    Added first element to implement data structure which process different cells

--- katas/test_dungeon.cpp
+++ katas/test_dungeon.cpp
@@ -5,32 +5,37 @@
 #include <ctime>
 
 
-//static std::pair<int,int> Empty
+//static Cell Empty
+
+struct Cell {
+	Cell(int id_=-1): id(id_) {};
+	int id;
+	std::vector<std::shared_ptr<Cell>> connectedCells;
+};
+
 
 
 //TODO check random door generator
 struct DoorGenerator {
-	virtual std::pair<std::pair<int,int>,std::pair<int,int>> make(int size)  = 0;
+	virtual std::pair<Cell,Cell> make(int size)  = 0;
 };
 
 struct FakeDoorGenerator: DoorGenerator {
-	std::pair<int,int> entrance;
-	std::pair<int,int> exit;
-	FakeDoorGenerator(std::pair<int,int> entrance_, std::pair<int,int> exit_): entrance(entrance_), exit(exit_) {};
-	std::pair<std::pair<int,int>,std::pair<int,int>> make(int size) {
+	Cell entrance;
+	Cell exit;
+	FakeDoorGenerator(Cell entrance_, Cell exit_): entrance(entrance_), exit(exit_) {};
+	std::pair<Cell, Cell> make(int size) {
 		return std::make_pair(entrance,exit); 
 	};
 };
 
-//TODO pending to test
+//TODO pending to test and finish
 struct RandomDoorGenerator: DoorGenerator {
-
-
 	RandomDoorGenerator() {
 	
 	};
 
-	std::pair<std::pair<int,int>,std::pair<int,int>> make(int size) {
+	std::pair<Cell,Cell> make(int size) {
 		std::srand(std::time(0));
 		int random_variable = (int)(std::rand() % size);
 		
@@ -45,19 +50,19 @@ struct Dungeon  {
 	int size;
 	std::shared_ptr<DoorGenerator> doorGenerator;
 
-	std::pair<int,int> entrance;
-	std::pair<int,int> exit;
+	Cell entrance;
+	Cell exit;
 	Dungeon(std::shared_ptr<DoorGenerator> & doorGenerator_, int size_=0): size(size_), doorGenerator(doorGenerator_){
-		std::pair<std::pair<int,int>, std::pair<int,int>> doors = doorGenerator->make(size);
+		std::pair<Cell, Cell> doors = doorGenerator->make(size);
 		this->entrance = doors.first;
 		this->exit = doors.second;
 	};
 
- 	std::pair <int,int> getExit() {
+ 	Cell getExit() {
 		return this->exit;
 	}
 
-	std::pair <int,int> getEnter() {
+	Cell getEnter() {
 		return this->entrance;
 	}
 
@@ -65,8 +70,8 @@ struct Dungeon  {
 	
 	}
 
-	std::pair<int,int> getPlace() {
-		return std::make_pair(0,0);
+	Cell getPlace() {
+		return Cell(0);
 	}
 
 };
@@ -75,35 +80,26 @@ struct Dungeon  {
 //TODO Check incorrect dungeons (negative)
 
 TEST_CASE ("An empty dungeon is initialised correctly", "[empty_dungeon]")  {
-	std::shared_ptr<DoorGenerator> fakeDoorGenerator =  std::make_shared<FakeDoorGenerator>(std::make_pair(0,0),std::make_pair(0,0));
+	std::shared_ptr<DoorGenerator> fakeDoorGenerator =  std::make_shared<FakeDoorGenerator>(Cell(0),Cell(0));
 	Dungeon dungeon(fakeDoorGenerator,1);
-	std::pair<int,int> exit = dungeon.getExit();
-	REQUIRE (exit.first == 0);
-	REQUIRE (exit.second == 0);
+	Cell exit = dungeon.getExit();
+	REQUIRE (exit.id == 0);
 }
 
 TEST_CASE("A dungeon should","[dungeon]") {
-	std::shared_ptr<DoorGenerator> doorGenerator =  std::make_shared<FakeDoorGenerator>(std::make_pair(0,0),std::make_pair(1,1));
+	std::shared_ptr<DoorGenerator> doorGenerator =  std::make_shared<FakeDoorGenerator>(Cell(0),Cell(1));
 	Dungeon dungeon(doorGenerator,2);
 	SECTION("with a size >=2, contains and enter and exit") {
-
-		std::pair<int,int> enter = dungeon.getEnter();
-		REQUIRE(enter.first==0);
-		REQUIRE(enter.second==0);
-		std::pair<int,int>  exit = dungeon.getExit();
-		REQUIRE(exit.first==1);
-		REQUIRE(exit.second==1);
+		Cell enter = dungeon.getEnter();
+		REQUIRE(enter.id==0);
+		Cell  exit = dungeon.getExit();
+		REQUIRE(exit.id==1);
 	}
 
 	SECTION("with permits enter a new explorer in the position 0, 0") {
 		dungeon.enter();
-		std::pair<int,int> posic = dungeon.getPlace();
-		REQUIRE(posic.first==0 );
-		REQUIRE(posic.second==0 );
-
-		
-
-
+		Cell posic = dungeon.getPlace();
+		REQUIRE(posic.id==0 );
 
 	}
 
