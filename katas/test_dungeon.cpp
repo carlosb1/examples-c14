766f9e08260bd1b661b3f433497d7066efaf6548
Carlos B <carlos.baezruiz@gmail.com>
2017-06-21 23:48:48 +0200

    Added first step to implement a different builder which it will permit to select different doors and the creation of a fake object easily

--- katas/test_dungeon.cpp
+++ katas/test_dungeon.cpp
@@ -20,35 +20,51 @@ enum door {
 	open=1,
 };
 
+struct BuilderDungeon {
 
+	std::vector<int> orderCells;
 
-struct Dungeon  {
-	int numberCells;
-	
-	std::shared_ptr<Cell>  entrance;
-	std::shared_ptr<Cell>  exit;
-	std::shared_ptr<Cell> current;
-	bool foundExit;
-	int countId=0;
-
-	//TODO numberCells refactorization!
-	Dungeon( int numberCells_=0): numberCells(numberCells_){
-		this->entrance  = std::make_shared<Cell>(countId);
+	BuilderDungeon(std::vector<int> orderCells_): orderCells(orderCells_) {};
+	std::pair<std::shared_ptr<Cell>,std::shared_ptr<Cell>> build(int numberCells) {
+		int countId=0;
+		std::shared_ptr<Cell>  entrance;
+		entrance  = std::make_shared<Cell>(countId);
 		countId++;
-
-		this->foundExit = false;
-
+		//TODO refactor to a function
 		std::shared_ptr<Cell> parentCell = entrance;
 		std::shared_ptr<Cell> nextCell = entrance;
 		for (int index=0; index < numberCells; index++) {
-			nextCell = std::make_shared<Cell>(countId,1);
+			nextCell = std::make_shared<Cell>(countId,orderCells[index]);
 			countId++;
 			parentCell->connectedCells.push_back(nextCell);
 			parentCell = nextCell;
 		}
-		this->exit = nextCell;
+		return std::make_pair(entrance,nextCell);
+	}
+
+};
+
+
+struct Dungeon  {
+	int numberCells;
+	BuilderDungeon  builder;
+	std::shared_ptr<Cell>  entrance;
+	std::shared_ptr<Cell>  exit;
+	std::shared_ptr<Cell> current;
+	bool foundExit;
+
+	//TODO numberCells refactorization!
+	Dungeon(BuilderDungeon builder_ , int numberCells_=0): numberCells(numberCells_), builder(builder_){
+		this->foundExit = false;
+		
+		std::pair<std::shared_ptr<Cell>,std::shared_ptr<Cell>> values = builder.build(numberCells);	
+
+		this->entrance = values.first;
+		this->exit = values.second;
 		this->current = entrance;
 	};
+
+
 
 
  	
@@ -93,13 +109,18 @@ struct Dungeon  {
 //TODO Check incorrect dungeons (negative)
 
 TEST_CASE ("An empty dungeon is initialised correctly", "[empty_dungeon]")  {
-	Dungeon dungeon(1);
+	std::vector<int> orderCells  = {1};
+	BuilderDungeon builder(orderCells);
+	Dungeon dungeon(builder,1);
 	Cell exit = dungeon.getExit();
 	REQUIRE (exit.id == 1);
 }
 
 TEST_CASE("A dungeon with only two cells should","[dungeon]") {
-	Dungeon dungeon(2);
+	std::vector<int> orderCells  = {1,1};
+	BuilderDungeon builder(orderCells);
+	//TODO ncessary add size of cells in dungeon class
+	Dungeon dungeon(builder,2);
 	
 	SECTION("with a numberCells >=2, contains and enter and exit") {
 		Cell enter = dungeon.getEnter();
@@ -136,16 +157,30 @@ TEST_CASE("A dungeon with only two cells should","[dungeon]") {
  }
 
 TEST_CASE("A dungeon with three cells should","[dungeon]") {
-	Dungeon dungeon(3);
+	std::vector<int> orderCells  = {1,1,1};
+	BuilderDungeon builder(orderCells);
+	Dungeon dungeon(builder,3);
 
 	SECTION("with enters choose first and first door and it arrives to the end") {
 		dungeon.visit(1);
-		REQUIRE(dungeon.isExit()==false);
 		dungeon.visit(1);
 		dungeon.visit(1);
 		REQUIRE(dungeon.isExit()==true);
 	}
-	
+
+}
+TEST_CASE("A dungeon with three cells, two different decisions should","[dungeon]") {
+	std::vector<int> orderCells  = {1,2,1};
+	BuilderDungeon builder(orderCells);
+	Dungeon dungeon(builder,3);
+
+	SECTION("with enters choose first, second and first door and it arrives to the end") {
+		dungeon.visit(1);
+		dungeon.visit(2);
+		dungeon.visit(1);
+		REQUIRE(dungeon.isExit()==true);
+	}
+		//TODO test to choose one cell and rectify	
 
 }
 
