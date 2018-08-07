f382da9e29ed3003127d213f06460e384505f96c
Carlos B <carlos.baezruiz@gmail.com>
2018-08-07 19:36:44 +0200

    Updated parallel array

--- c_high_performance/example_parallel_array.cpp
+++ c_high_performance/example_parallel_array.cpp
@@ -13,8 +13,24 @@ struct BigObject {
 	std::array<char, 256> data_{};
 	int score_{};
 };
+template<class T> auto sum_scores(const std::vector<T> & objects) {
+	ScopedTimer t{"sum_scores"};
+
+	auto sum = 0;
+	for (const auto& obj: objects) {
+		sum +=obj.score_;
+	}
+	return sum;
+}
 int main () {
+
+	auto small_objects = std::vector<SmallObject>(1'000'000);
+	auto big_objects = std::vector<BigObject>(1'000'000);
+
 	std::cout << sizeof(SmallObject);
 	std::cout << sizeof(BigObject);
+	auto sum = 0;
+	sum += sum_scores(small_objects);
+	sum += sum_scores(big_objects);
 
 }
