aa766f02eef9182fdd7e617fede9a005229a47fa
Carlos B <carlos.baezruiz@gmail.com>
2018-11-24 18:56:52 +0100

    Added example for corroutines

---
+++ coroutines/example.cpp
@@ -0,0 +1,62 @@
+#include "coroutine.h"
+#include <iostream>
+#include <chrono>
+#include <functional>
+
+coroutine::Channel<int> channel;
+
+std::string async_func() {
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+	return "22";
+}
+
+void routine_func1() {
+	int i = channel.pop();
+	std::cout << i << std::endl;
+}
+
+void routine_func2(int i) {
+	std::cout << "20" << std::endl;
+	coroutine::yield();
+	std::cout << "21" << std::endl;
+	
+	std::string str = coroutine::await(async_func);
+	std::cout << str << std::endl;
+}
+
+void thread_func () {
+	coroutine::routine_t rt1 = coroutine::create(routine_func1);
+	coroutine::routine_t rt2 = coroutine::create(std::bind(routine_func2, 2));
+	std::cout << "00" << std::endl;
+	coroutine::resume(rt1);
+
+
+	std::cout << "02" << std::endl;
+	channel.push(10);
+
+
+	std::cout << "03" << std::endl;
+	coroutine::resume(rt2);
+	
+	std::cout << "04" << std::endl;
+	channel.push(11);
+
+	std::cout << "05" << std::endl;
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(6000));
+
+	coroutine::resume(rt2);
+
+	
+	coroutine::destroy(rt1);
+	coroutine::destroy(rt2);
+}
+
+int main() {
+	std::thread t1(thread_func);
+	std::thread t2([]() {
+			});
+	t1.join();
+	t2.join();
+}
+
