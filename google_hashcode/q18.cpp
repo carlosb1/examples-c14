20f0ccd11c96e8dc28d0f77acf323cc403c3083c
Carlos B <carlos.baezruiz@gmail.com>
2019-03-08 21:10:52 +0100

    added first car specification

--- google_hashcode/q18.cpp
+++ google_hashcode/q18.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <memory>
 
+struct Car {
+	int point_start;
+	int point_stop;
+
+	Car(int point_start_, int point_stop_): point_start(point_start_), point_stop(point_stop_) {}; 
+	static std::shared_ptr<Car> newCar(int point_start_, int point_stop_) {
+		return std::make_shared<Car>(point_start_, point_stop_);
+	}
+};
+
+
+void print_matrix(std::vector<std::vector<int>> & matrix) {
+	for (auto row: matrix) {
+		for (auto elem: row) {
+			std::cout << elem;
+		}
+		std::cout << "\n";
+	}
+
+}
 int main () {
 	int rows = 4;
 	int cols = 4;
 	
-	std::vector<std::vector<int>> matrix(rows);
-	auto func_init = [cols](auto vec) { 
-		std::cout << "hello!";
-		vec.assign(cols,0); };
-	for_each(matrix.begin(), matrix.end(), func_init);
-
-	for (auto rows: matrix) {
-		std::cout << "row\n";
-		for (auto elem: rows) {
-			std::cout << "col\n";
-			std::cout << elem <<"\n";
-		}
-	}	
+	std::vector<std::vector<int>> matrix(rows, std::vector<int>(cols, 0));
 	
+	auto car = Car::newCar(1,1);	
+
+	//std::for_each(std::begin(matrix), std::end(matrix), [] (auto & v) mutable { for (auto& e: v) std::cin >> e;});
 
 
 }
