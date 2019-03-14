1db742781a6bfcc5d50cfbd1fed16678399dfcce
Carlos B <carlos.baezruiz@gmail.com>
2019-03-14 17:57:39 +0100

    Added function to create new cars

--- google_hashcode/q18_2.cpp
+++ google_hashcode/q18_2.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <cmath>
 #include <utility>
+#include <iterator>
 
 
 struct Car {
@@ -53,25 +54,41 @@ double calculate_heuristic(Map & map) {
 	return h;
 }
 
-std::vector<std::vector<int>> generate_new_poses(auto point_current) {
+std::vector<std::vector<int>> generate_new_poses(auto point_current, int rows, int cols) {
 	std::vector<std::vector<int>> new_poses;
 	for (int i = -1; i < 2; i++) {
 		for (int j = -1; j++; j < 2) {
-			if ((point_current[0] + i) >= 0 && (point_current[0] + i)  < this->rows && (point_current[1] + j)  >= 0 && (point_current[1] + j)  < this->cols) {
-				if ( i==0 && j==0) {
-					continue;
-				}
+			if ((point_current[0] + i) >= 0 && (point_current[0] + i)  < rows && (point_current[1] + j)  >= 0 && (point_current[1] + j)  < cols) {
+				//if ( i==0 && j==0) {
+				//	continue;
+				//}
 				new_poses.push_back({point_current[0]+i,point_current[1]+j});
 			}
 		}
 	}
 	return new_poses;
 }
+std::vector<Car> generate_moved_cars(auto car, auto poses) {
+	auto func_new_car = [car] (auto pose) {
+		auto new_car = Car{car.point_start, car.point_stop};
+		new_car.point_current = {car.point_current[0]+pose[0], car.point_current[1]+pose[1]};
+		return new_car;
+	};
+	//std::vector<Car> cars = std::transform(poses.begin(), poses.end(), std::back_inserter(poses), func_new_car);		
+	return cars;
+}
 
-std::vector<Map> generate_new_maps(Map & map) {
-	for (auto car: map.cars) {
-		
+std::vector<Map> generate_new_maps(Map & map, int rows, int cols) {
+	std::vector<Map> new_maps;
+	for (int index_car = 0; index_car < map.cars.size(); index_car++) {
+		auto new_poses = generate_new_poses(map.cars[index_car].point_current, rows, cols);
+		auto moved_cars = generate_moved_cars(map.cars[index_car], new_poses);
 	}
+	return new_maps;
+	//for (auto car: map.cars) {
+	//	auto new_poses_car = generate_new_poses(car.point_current);
+		//new_maps.push_back();
+	//}
 	
 
 	//add new combinations but we avoid repeat
@@ -83,7 +100,6 @@ int main() {
 	auto car = Car(std::vector{0, 0}, std::vector{5, 5});
 	auto car2 = Car(std::vector{2, 2}, std::vector{7, 7});
 	auto init_case = {car, car2};
-	std::cout << "hi my friend\n";	
 	auto map = Map{init_case};
 	std::cout << "heuristic: "<< calculate_heuristic(map) << "\n";
 	maps.push_back(map);	
